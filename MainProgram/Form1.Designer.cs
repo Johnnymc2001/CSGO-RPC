e8debf52e8612a038fa24af6713cd3ebaad937e6
Johnnymc2001 <37579854+Johnnymc2001@users.noreply.github.com>
2021-11-30 20:58:30 +0700

    Add project files.

---
+++ MainProgram/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace MainProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbAutoStart = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabCSGO_btnInstall = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabLobby_btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabLobby_txtState = new System.Windows.Forms.TextBox();
+            this.tabLobby_txtDetail = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabIngame_btnSave = new System.Windows.Forms.Button();
+            this.tabIngame_cbShowGamemode = new System.Windows.Forms.CheckBox();
+            this.tabIngame_cbShowMap = new System.Windows.Forms.CheckBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(11, 264);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // cbAutoStart
+            // 
+            this.cbAutoStart.AutoSize = true;
+            this.cbAutoStart.Location = new System.Drawing.Point(92, 268);
+            this.cbAutoStart.Name = "cbAutoStart";
+            this.cbAutoStart.Size = new System.Drawing.Size(68, 17);
+            this.cbAutoStart.TabIndex = 4;
+            this.cbAutoStart.Text = "Autostart";
+            this.cbAutoStart.UseVisualStyleBackColor = true;
+            this.cbAutoStart.CheckedChanged += new System.EventHandler(this.cbAutoStart_CheckedChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(498, 250);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tabCSGO_btnInstall);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(490, 224);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "CS:GO";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabCSGO_btnInstall
+            // 
+            this.tabCSGO_btnInstall.Location = new System.Drawing.Point(409, 6);
+            this.tabCSGO_btnInstall.Name = "tabCSGO_btnInstall";
+            this.tabCSGO_btnInstall.Size = new System.Drawing.Size(75, 23);
+            this.tabCSGO_btnInstall.TabIndex = 6;
+            this.tabCSGO_btnInstall.Text = "Install";
+            this.tabCSGO_btnInstall.UseVisualStyleBackColor = true;
+            this.tabCSGO_btnInstall.Click += new System.EventHandler(this.tabCSGO_btnInstall_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(296, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "This program require custom Gamestate Intergration CFG File.\r\nClick Install to co" +
+    "py the Gamestate Intergration CFG\r\nThe program use Port 4123 ";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tabLobby_btnSave);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.tabLobby_txtState);
+            this.tabPage2.Controls.Add(this.tabLobby_txtDetail);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(490, 224);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Lobby";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabLobby_btnSave
+            // 
+            this.tabLobby_btnSave.Location = new System.Drawing.Point(409, 195);
+            this.tabLobby_btnSave.Name = "tabLobby_btnSave";
+            this.tabLobby_btnSave.Size = new System.Drawing.Size(75, 23);
+            this.tabLobby_btnSave.TabIndex = 12;
+            this.tabLobby_btnSave.Text = "Save";
+            this.tabLobby_btnSave.UseVisualStyleBackColor = true;
+            this.tabLobby_btnSave.Click += new System.EventHandler(this.tabLobby_btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "When your are not in a match";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 182);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(174, 39);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Some Placeholder You Can Use\r\n{USERNAME} - Your steam\'s name\r\n{ID} - Your steam\'s" +
+    " ID\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "State";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Details";
+            // 
+            // tabLobby_txtState
+            // 
+            this.tabLobby_txtState.Location = new System.Drawing.Point(77, 69);
+            this.tabLobby_txtState.Name = "tabLobby_txtState";
+            this.tabLobby_txtState.Size = new System.Drawing.Size(407, 20);
+            this.tabLobby_txtState.TabIndex = 1;
+            // 
+            // tabLobby_txtDetail
+            // 
+            this.tabLobby_txtDetail.Location = new System.Drawing.Point(77, 40);
+            this.tabLobby_txtDetail.Name = "tabLobby_txtDetail";
+            this.tabLobby_txtDetail.Size = new System.Drawing.Size(407, 20);
+            this.tabLobby_txtDetail.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.tabIngame_btnSave);
+            this.tabPage3.Controls.Add(this.tabIngame_cbShowGamemode);
+            this.tabPage3.Controls.Add(this.tabIngame_cbShowMap);
+            this.tabPage3.Controls.Add(this.label9);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(490, 224);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Ingame";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabIngame_btnSave
+            // 
+            this.tabIngame_btnSave.Location = new System.Drawing.Point(412, 198);
+            this.tabIngame_btnSave.Name = "tabIngame_btnSave";
+            this.tabIngame_btnSave.Size = new System.Drawing.Size(75, 23);
+            this.tabIngame_btnSave.TabIndex = 15;
+            this.tabIngame_btnSave.Text = "Save";
+            this.tabIngame_btnSave.UseVisualStyleBackColor = true;
+            this.tabIngame_btnSave.Click += new System.EventHandler(this.tabIngame_btnSave_Click);
+            // 
+            // tabIngame_cbShowGamemode
+            // 
+            this.tabIngame_cbShowGamemode.AutoSize = true;
+            this.tabIngame_cbShowGamemode.Location = new System.Drawing.Point(9, 45);
+            this.tabIngame_cbShowGamemode.Name = "tabIngame_cbShowGamemode";
+            this.tabIngame_cbShowGamemode.Size = new System.Drawing.Size(110, 17);
+            this.tabIngame_cbShowGamemode.TabIndex = 14;
+            this.tabIngame_cbShowGamemode.Text = "Show Gamemode";
+            this.tabIngame_cbShowGamemode.UseVisualStyleBackColor = true;
+            // 
+            // tabIngame_cbShowMap
+            // 
+            this.tabIngame_cbShowMap.AutoSize = true;
+            this.tabIngame_cbShowMap.Location = new System.Drawing.Point(9, 22);
+            this.tabIngame_cbShowMap.Name = "tabIngame_cbShowMap";
+            this.tabIngame_cbShowMap.Size = new System.Drawing.Size(77, 17);
+            this.tabIngame_cbShowMap.TabIndex = 13;
+            this.tabIngame_cbShowMap.Text = "Show Map";
+            this.tabIngame_cbShowMap.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(6, 3);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(181, 16);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "When your are in a match";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 299);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.cbAutoStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "CSGO - Discord Rick Presence";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbAutoStart;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button tabCSGO_btnInstall;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button tabLobby_btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tabLobby_txtState;
+        private System.Windows.Forms.TextBox tabLobby_txtDetail;
+        private System.Windows.Forms.Button tabIngame_btnSave;
+        private System.Windows.Forms.CheckBox tabIngame_cbShowGamemode;
+        private System.Windows.Forms.CheckBox tabIngame_cbShowMap;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ MainProgram/Form1.cs
@@ -0,0 +1,340 @@
+﻿using CSGSI;
+using DiscordRPC;
+using Gameloop.Vdf;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Configuration = System.Configuration.Configuration;
+
+namespace MainProgram
+{
+    public partial class Form1 : Form
+    {
+        Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+
+        GameStateListener gsl = new GameStateListener(4123);
+        DiscordRpcClient client = new DiscordRpcClient("494378226857279498", -1);
+
+        public static class IdleSetting
+        {
+            public static string Detail { get; set; } = "Idle";
+            public static string State { get; set; } = "Chillin in the menu";
+        }
+
+        public static class IngameSetting
+        {
+            public static bool ShowMap { get; set; } = true;
+            public static bool ShowGamemode { get; set; } = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // =========================================================== Main Function ===========================================================
+
+        private void SetConfig(string key, string value)
+        {
+            RemoveConfig(key);
+            config.AppSettings.Settings.Add(key, value);
+            config.Save(ConfigurationSaveMode.Minimal);
+        }
+
+        private string GetConfig(string key)
+        {
+            return config.AppSettings.Settings[key]?.Value;
+        }
+
+        private void RemoveConfig(string key)
+        {
+            if (GetConfig(key) != null)
+                config.AppSettings.Settings.Remove(key);
+        }
+
+        private void InitService()
+        {
+            try
+            {
+                //client.Invoke();
+                client.Initialize();
+                gsl.NewGameState += OnNewGameState;
+                gsl.Start();
+
+                btnStart.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                DeInitService();
+
+                btnStart.Enabled = true;
+                MessageBox.Show("Failed to run, please try again!", ex.Message);
+
+            }
+        }
+
+        private void DeInitService()
+        {
+            if (client.IsInitialized)
+            {
+                client.Deinitialize();
+            }
+
+            if (!client.IsDisposed)
+            {
+                client.Dispose();
+            }
+
+            if (gsl.Running)
+            {
+                gsl.Stop();
+            }
+        }
+        // ================================================================ GameStates ================================================================
+
+        private void OnNewGameState(GameState gs)
+        {
+            var player = gs.Player;
+            var activity = player.Activity;
+
+            //Trace.WriteLine(gs.JSON);
+            if (activity.ToString().ToLower() == "menu")
+            {
+                var name = player.Name;
+                var id = player.SteamID;
+
+                IdleSetting.Detail = IdleSetting.Detail.Replace("{USERNAME}", name).Replace("{ID}", id);
+                IdleSetting.State = IdleSetting.State.Replace("{USERNAME}", name).Replace("{ID}", id);
+
+                UpdatePresence(IdleSetting.Detail, IdleSetting.State, "csgo", "Playing CSGO", "csgo", "Playing CSGO");
+            }
+            else if (activity.ToString().ToLower() == "playing")
+            {
+                var game = gs.Map;
+
+                string map = game.Name;
+                string mode = game.Mode.ToString();
+                string phase = gs.Round.Phase.ToString();
+
+                var teamT = game.TeamT;
+                var teamCT = game.TeamCT;
+
+                string detail = "";
+                string largeKey = "";
+
+                if (IngameSetting.ShowGamemode) detail += mode;
+                if (IngameSetting.ShowMap) { detail += detail != "" ? " - " + map : map; largeKey = $"{map}"; }
+
+                string teamTStr = $"[𝗧 {teamT.Score}]";
+                string teamCTStr = $"[{teamCT.Score} 𝗖𝗧]";
+
+                string smallKey = player.Team.ToString() == "T" ? "terrorists" : "counterterrorists" ?? "spec";
+                string smallText = $"{player.Name}";
+
+                UpdatePresence($"{detail}", $"{teamTStr} ({phase}) {teamCTStr}", largeKey, largeKey, smallKey, smallText);
+            }
+            //Trace.Write(gs.JSON);
+        }
+
+        private void UpdatePresence(
+            string title, string detail,
+            string largeKey = "none", string largeText = "",
+            string smallKey = "none", string smallText = "")
+        {
+            var curPres = client.CurrentPresence;
+            var pres = new RichPresence()
+            {
+                Details = title,
+                State = detail,
+                Assets = new Assets()
+                {
+                    LargeImageKey = largeKey,
+                    LargeImageText = largeText,
+                    SmallImageKey = smallKey,
+                    SmallImageText = smallText,
+                }
+            };
+
+            if (curPres != null)
+            {
+                if (curPres != pres)
+                {
+                    client.SetPresence(pres);
+                }
+            }
+            else
+            {
+                client.SetPresence(pres);
+            }
+
+        }
+
+        // =========================================================== Main Form Events ===========================================================
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            InitService();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason != CloseReason.UserClosing)
+                return;
+
+            DialogResult f;
+            f = MessageBox.Show("Are you sure you want to exit?", "CSGO - Discord Rich Presence", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (f == DialogResult.OK)
+            {
+                DeInitService();
+                Environment.Exit(0);
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void cbAutoStart_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbAutoStart.Checked)
+            {
+                SetConfig("AutoStart", "True");
+            }
+            else
+            {
+                SetConfig("AutoStart", "False");
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Init for Lobby
+            IdleSetting.Detail = GetConfig("Lobby_Detail");
+            IdleSetting.State = GetConfig("Lobby_State");
+
+            tabLobby_txtDetail.Text = IdleSetting.Detail;
+            tabLobby_txtState.Text = IdleSetting.State;
+
+            // Init For Ingame
+            IngameSetting.ShowMap = GetConfig("Ingame_ShowMap") == "True";
+            IngameSetting.ShowGamemode = GetConfig("Ingame_ShowGamemode") == "True";
+
+            tabIngame_cbShowMap.Checked = IngameSetting.ShowMap;
+            tabIngame_cbShowGamemode.Checked = IngameSetting.ShowGamemode;
+            if (GetConfig("AutoStart") == "True")
+            {
+                // Init For Main
+                cbAutoStart.Checked = true;
+                InitService();
+            }
+        }
+
+        // ================================================================ Tab CSGO ================================================================
+
+        private void tabCSGO_btnInstall_Click(object sender, EventArgs e)
+        {
+            object steamReg = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam", "InstallPath", new object()) ?? Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", new object());
+
+            var pathToLibraryFile = $@"{steamReg}\steamapps\libraryfolders.vdf";
+            var cfgFolder = "";
+            bool found = false;
+
+            if (File.Exists(pathToLibraryFile))
+            {
+                string[] lines = File.ReadAllLines(pathToLibraryFile);
+
+                foreach (string line in lines)
+                {
+                    if (line.Contains(@"path"))
+                    {
+                        cfgFolder = line.Replace("\"", "").Replace("path", "").Replace("\\\\", "\\").Trim();
+                    }
+                    else if (line.Contains("\"730\""))
+                    {
+                        found = true;
+                        cfgFolder += @"\steamapps\common\Counter-Strike Global Offensive\csgo\cfg";
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    if (File.Exists($@"{cfgFolder}\gamestate_integration_jrpc.cfg"))
+                    {
+                        MessageBox.Show("The program have already been installed!", "CSGO - Discord Rich Presence");
+                    }
+                    else
+                    {
+                        string cfg = @"""CSGSI"" 
+{ 
+""uri"" ""http://localhost:4123"" 
+""timeout"" ""5.0"" 
+""buffer"" ""0.1""
+""heartbeat"" ""15.0""
+""data"" 
+    { 
+        ""provider"" ""1"" 
+        ""map"" ""1"" 
+        ""round"" ""1"" 
+        ""player_id"" ""1"" 
+        ""player_weapons"" ""1"" 
+        ""player_match_stats"" ""1"" 
+        ""player_state"" ""1"" 
+        ""allplayers_id"" ""1"" 
+        ""allplayers_state"" ""1"" 
+        ""allplayers_match_stats"" ""1"" 
+    } 
+}";
+
+                        File.WriteAllText($@"{cfgFolder}\gamestate_integration_jrpc.cfg", cfg);
+                        MessageBox.Show("The program installed sucessfully!", "CSGO - Discord Rich Presence");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Auto-Install Failed. You can follow the steps in the readme to do it manually!", "CSGO - Discord Rich Presence");
+                }
+            }
+        }
+
+        // ================================================================ Tab Lobby ================================================================
+        private void tabLobby_btnSave_Click(object sender, EventArgs e)
+        {
+            string detail = tabLobby_txtDetail.Text;
+            string state = tabLobby_txtState.Text;
+
+            SetConfig("Lobby_Detail", detail);
+            SetConfig("Lobby_State", state);
+
+            IdleSetting.Detail = detail;
+            IdleSetting.State = state;
+
+            MessageBox.Show("Setting saved!");
+        }
+
+        private void tabIngame_btnSave_Click(object sender, EventArgs e)
+        {
+            bool ShowMap = tabIngame_cbShowMap.Checked ? true : false;
+            bool ShowGamemode = tabIngame_cbShowGamemode.Checked ? true : false;
+
+            SetConfig("Ingame_ShowMap", ShowMap == true ? "True" : "False");
+            SetConfig("Ingame_ShowGamemode", ShowGamemode == true ? "True" : "False");
+
+            IngameSetting.ShowMap = ShowMap;
+            IngameSetting.ShowGamemode = ShowGamemode;
+
+            MessageBox.Show("Setting saved!");
+        }
+    }
+}
