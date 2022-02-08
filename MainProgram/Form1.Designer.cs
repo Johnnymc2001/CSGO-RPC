ffd6d84e6f5c83fba5f4384a185331129d341db5
Johnnymc2001 <37579854+Johnnymc2001@users.noreply.github.com>
2022-02-08 13:21:34 +0700

    Update icon

--- MainProgram/Form1.Designer.cs
+++ MainProgram/Form1.Designer.cs
@@ -660,6 +660,7 @@
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- MainProgram/Properties/AssemblyInfo.cs
+++ MainProgram/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MainProgram")]
+[assembly: AssemblyTitle("CSGO-RPC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MainProgram")]
+[assembly: AssemblyCompany("Johnny")]
+[assembly: AssemblyProduct("CSGO-RPC")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
